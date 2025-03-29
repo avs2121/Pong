9c72fa29a84591e35a42b857bc819a93a6b3b841
Aksel Schiøtz <schiotzaksel@gmail.com>
2025-03-29 08:51:40 +0100

    Added logic to main

--- PongSolution/Pong/main.cpp
+++ PongSolution/Pong/main.cpp
@@ -2,6 +2,7 @@
 #include "Ball.h"
 #include <sstream>
 #include <cstdlib>
+#include <conio.h>
 #include <SFML/Graphics.hpp>
 
 using namespace sf; 
@@ -21,5 +22,51 @@ int main() {
 	Text hud; 
 	Font font; 
 	font.loadFromFile("digital-7.ttf");
+	hud.setFont(font);
+	hud.setCharacterSize(75);
+	hud.setFillColor(sf::Color::White);
+
+	while (window.isOpen()) {
+
+
+		Event event; 
+		while (window.pollEvent(event)) {
+			if (event.type == Event::Closed) {
+				window.close();
+			}
+			if (Keyboard::isKeyPressed(Keyboard::Left)) {
+				bat.moveLeft();
+			}
+			else if (Keyboard::isKeyPressed(Keyboard::Right)) {
+				bat.moveRight();
+			}
+			else if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+				window.close();
+			}
+		}
+
+		if (ball.getBallPosition().top > windowHeight) {
+			ball.hitBottom();
+			if (lives >= 1) {
+				lives--;
+			}
+			else {
+				score = 0; 
+				lives = 3; 
+			}
+		}
+		if (ball.getBallPosition().top < 0) {
+			ball.reboundBatOrTop();
+			score++; 
+		}
+		if (ball.getBallPosition().left + 10 > windowWidth || ball.getBallPosition().left < 0) {
+			ball.reboundSides();
+		}
+		if (ball.getBallPosition().intersects(bat.getPosition())) {
+			ball.reboundBatOrTop();
+		}
+	}
+
+
 
 }
