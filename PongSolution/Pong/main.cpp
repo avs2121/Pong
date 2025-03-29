9833a7dce0c3c22bb0b17010c0a1f45b8983f8c9
Aksel Schiøtz <schiotzaksel@gmail.com>
2025-03-29 08:33:36 +0100

    Sat up the window in main

--- PongSolution/Pong/main.cpp
+++ PongSolution/Pong/main.cpp
@@ -7,5 +7,19 @@
 using namespace sf; 
 
 int main() {
+	int windowWidth = 1024; 
+	int windowHeight = 768; 
+
+	RenderWindow window(VideoMode(windowWidth, windowHeight), "Pong");
+
+	int score = 0; 
+	int lives = 3; 
+
+	Bat bat(windowWidth / 2, windowHeight - 20);
+	Ball ball(windowWidth / 2, 1);
+
+	Text hud; 
+	Font font; 
+	font.loadFromFile("digital-7.ttf");
 
 }
